17ee2d621b75c80e73baccb6c0160f65a2af7cdb
Ruben Ernst <57605025+RubenErnst@users.noreply.github.com>
2020-10-23 15:56:26 +0100

    Prepare data and figures for chapter 7

---
+++ code/Current year G20 measures/0 data prep.R
@@ -0,0 +1,119 @@
+rm(list = ls())
+
+### REQUEST:
+# 1. Please produce a line chart for the years 2009 to 2020 with the following variable on the primary vertical axis: Total number of G20 commercial policy
+# interventions worldwide documented by 21 October. On the second axis: please plot the percentage of G20 commercial policy interventions that are harmful.
+# For the primary axis title use the text highlighted in bold. For the secondary axis title please use “Percentage of G20 measures that harm trading partners.”
+#
+# 2. For each G20 country please add up the total number of harmful measures implemented this year, the total number of liberalising measures implemented this
+# year, and the total number of contingent protection investigations started this year but which have not resulted in duties imposed. Rank the G20 nations by
+# these totals. Please produce a 2-D bar chart with G20 country as a separate entry where red is used to indicate the number of harmful interventions, green is
+# used to indicate the liberalising interventions, and yellow to indicate the contingent protection investigations started this year. Please indicate in the chart
+# the number of measures in each category for each G20 member. So for each G20 member, the number associated with each colour segment in the bar will be reported.
+#
+# 3. For each G20 member, focusing only on the harmful interventions implemented this year, please produce a 2-D 100% stacked bar which indicates the % of measures
+# that have already lapsed (by 21 October), the % of measures due to lapse during the remainder of this year, the % of measures due to lapse in 2021, the % of
+# measures due to lapse after 2021, and the % of measures without phase out dates.
+
+library(gtalibrary)
+library(tidyverse)
+library(openxlsx)
+library(splitstackshape)
+
+gta_setwd()
+gta26.path = "0 report production/GTA 26/"
+data.path = "data/Current year G20 measures/"
+source(paste0(gta26.path, "help files/GTA 26 cutoff and definitions.R"))
+
+
+### Table 1
+# Get data
+gta_data_slicer(implementing.country = country.names$un_code[country.names$is.g20],
+                keep.implementer = T)
+
+# Keep only interventions documented by 21 Oct
+master.sliced <- subset(master.sliced, format(as.Date(date.published), "%m-%d") <= "10-21")
+
+# Aggregate per year and add percentage of harmful interventions
+table1 <- merge(select(aggregate(intervention.id ~ year(date.published), master.sliced, function(x){length(unique(x))}), "year.published" = "year(date.published)", "total.nr.of.int" = intervention.id),
+                select(aggregate(intervention.id ~ year(date.published), subset(master.sliced, gta.evaluation %in% c("Red", "Amber")), function(x){length(unique(x))}), "year.published" = "year(date.published)", "nr.of.harmful.interventions" = intervention.id),
+                by = "year.published", all.x = T)
+
+table1$harmful.perc <- table1$nr.of.harmful.interventions / table1$total.nr.of.int
+table1$nr.of.harmful.interventions <- NULL
+
+
+### Table 2
+# Get data
+gta_data_slicer(implementing.country = country.names$un_code[country.names$is.g20],
+                keep.implementer = T)
+
+# Convert gta.evaluation into harmful and liberalising
+master.sliced$gta.evaluation <- ifelse(master.sliced$gta.evaluation %in% c("Red", "Amber"), "harmful", "liberalising")
+
+# Aggregate the harmful and liberalising interventions for every country
+table2 <- merge(select(aggregate(intervention.id ~ implementing.jurisdiction, subset(master.sliced, gta.evaluation == "harmful" & !is.na(date.implemented) & date.implemented >= as.Date("2020-01-01") & date.implemented <= as.Date(cutoff.date)), function(x){length(unique(x))}), implementing.jurisdiction, "nr.of.harmful.int" = intervention.id),
+                select(aggregate(intervention.id ~ implementing.jurisdiction, subset(master.sliced, gta.evaluation == "liberalising" & !is.na(date.implemented) & date.implemented >= as.Date("2020-01-01") & date.implemented <= as.Date(cutoff.date)), function(x){length(unique(x))}), implementing.jurisdiction, "nr.of.liberalising.int" = intervention.id),
+                by = "implementing.jurisdiction", all.x = T)
+
+# Aggregate chapter D investigations that have not resulted in duties imposed
+# investigation.status.history <- read_delim("data/database replica/gta_investigation_status_history.csv", delim = ",")
+# investigation.status <- bar <- read_delim("data/database replica/gta_investigation_status.csv", delim = ",")
+# 
+# investigation.status.history <- merge(investigation.status.history, investigation.status, by.x = "status_id", by.y = "id", all.x = T)
+# data2 <- merge(subset(master.sliced, mast.chapter == "D" & !is.na(date.implemented) & date.implemented >= as.Date("2020-01-01") & date.implemented <= as.Date(cutoff.date)), investigation.status.history, by.x = "intervention.id", by.y = "intervention_id", all.x = T)
+# data2 <- select(aggregate(intervention.id ~ implementing.jurisdiction, subset(data2, !label %in% c("preliminary duty", "definitive duty", "extended duty")), function(x){length(unique(x))}), implementing.jurisdiction, "nr.of.cont.protection.int.RE" = intervention.id)
+# table2 <- merge(table2, data2, by = "implementing.jurisdiction", all.x = T)
+
+data2 <- select(aggregate(intervention.id ~ implementing.jurisdiction, subset(master.sliced, mast.chapter == "D" & date.announced >= as.Date("2020-01-01") & date.announced <= as.Date(cutoff.date) & is.na(date.implemented)), function(x){length(unique(x))}), implementing.jurisdiction, "nr.of.cont.protection.int" = intervention.id)
+table2 <- merge(table2, data2, by = "implementing.jurisdiction", all.x = T)
+
+# Merge the chapter D numbers with the rest
+# table2 <- merge(table2, data2, by = "implementing.jurisdiction", all.x = T); rm(data2, investigation.status, investigation.status.history)
+table2[is.na(table2)] <- 0
+
+# Add column of total interventions per country to rank the countries in the bar chart
+table2$total.nr.of.interventions <- table2$nr.of.harmful.int + table2$nr.of.liberalising.int + table2$nr.of.cont.protection.int
+
+
+### Table 3
+# Get data
+gta_data_slicer(gta.evaluation = c("Red", "Amber"),
+                implementation.period = c(as.Date("2020-01-01"), as.Date(cutoff.date)),
+                keep.implementation.na = F,
+                implementing.country = country.names$un_code[country.names$is.g20],
+                keep.implementer = T)
+
+# Aggregate total number of interventions per country and number of already lapsed interventions
+table3 <- merge(select(aggregate(intervention.id ~ implementing.jurisdiction, master.sliced, function(x){length(unique(x))}), implementing.jurisdiction, "total.nr.of.interventions" = intervention.id),
+                select(aggregate(intervention.id ~ implementing.jurisdiction, subset(master.sliced, !is.na(date.removed) & date.removed <= as.Date(cutoff.date)), function(x){length(unique(x))}), implementing.jurisdiction, "perc.of.lapsed.interventions" = intervention.id),
+                by = "implementing.jurisdiction", all.x = T)
+
+# Add number of interventions due to lapse during the rest of the year
+table3 <- merge(table3, select(aggregate(intervention.id ~ implementing.jurisdiction, subset(master.sliced, !is.na(date.removed) & date.removed > as.Date(cutoff.date) & date.removed <= as.Date("2020-12-31")), function(x){length(unique(x))}), implementing.jurisdiction, "perc.of.interventions.to.lapse.rest.of.year" = intervention.id),
+                by = "implementing.jurisdiction", all.x = T)
+
+# Add number of interventions due to lapse during 2021
+table3 <- merge(table3, select(aggregate(intervention.id ~ implementing.jurisdiction, subset(master.sliced, !is.na(date.removed) & date.removed >= as.Date("2021-01-01") & date.removed <= as.Date("2021-12-31")), function(x){length(unique(x))}), implementing.jurisdiction, "perc.of.interventions.to.lapse.2021" = intervention.id),
+                by = "implementing.jurisdiction", all.x = T)
+
+# Add number of interventions due to lapse after 2021
+table3 <- merge(table3, select(aggregate(intervention.id ~ implementing.jurisdiction, subset(master.sliced, !is.na(date.removed) & date.removed >= as.Date("2022-01-01")), function(x){length(unique(x))}), implementing.jurisdiction, "perc.of.interventions.to.lapse.after.2021" = intervention.id),
+                by = "implementing.jurisdiction", all.x = T)
+
+# Add number of interventions without removal date
+table3 <- merge(table3, select(aggregate(intervention.id ~ implementing.jurisdiction, subset(master.sliced, is.na(date.removed)), function(x){length(unique(x))}), implementing.jurisdiction, "perc.of.interventions.without.phaseout.date" = intervention.id),
+                by = "implementing.jurisdiction", all.x = T)
+
+# Convert everything to percentages and remove the column with total interventions
+table3[is.na(table3)] <- 0
+table3$perc.of.lapsed.interventions <- table3$perc.of.lapsed.interventions / table3$total.nr.of.interventions
+table3$perc.of.interventions.to.lapse.rest.of.year <- table3$perc.of.interventions.to.lapse.rest.of.year / table3$total.nr.of.interventions
+table3$perc.of.interventions.to.lapse.2021 <- table3$perc.of.interventions.to.lapse.2021 / table3$total.nr.of.interventions
+table3$perc.of.interventions.to.lapse.after.2021 <- table3$perc.of.interventions.to.lapse.after.2021 / table3$total.nr.of.interventions
+table3$perc.of.interventions.without.phaseout.date <- table3$perc.of.interventions.without.phaseout.date / table3$total.nr.of.interventions
+
+table3$total.nr.of.interventions <- NULL
+
+### Save data
+save(table1, table2, table3, file = paste0(gta26.path, data.path, "current year g20 measures.Rdata"))
---
+++ code/Current year G20 measures/1 create figures.R
@@ -0,0 +1,152 @@
+rm(list = ls())
+
+### REQUEST:
+# 1. Please produce a line chart for the years 2009 to 2020 with the following variable on the primary vertical axis: Total number of G20 commercial policy
+# interventions worldwide documented by 21 October. On the second axis: please plot the percentage of G20 commercial policy interventions that are harmful.
+# For the primary axis title use the text highlighted in bold. For the secondary axis title please use “Percentage of G20 measures that harm trading partners.”
+#
+# 2. For each G20 country please add up the total number of harmful measures implemented this year, the total number of liberalising measures implemented this
+# year, and the total number of contingent protection investigations started this year but which have not resulted in duties imposed. Rank the G20 nations by
+# these totals. Please produce a 2-D bar chart with G20 country as a separate entry where red is used to indicate the number of harmful interventions, green is
+# used to indicate the liberalising interventions, and yellow to indicate the contingent protection investigations started this year. Please indicate in the chart
+# the number of measures in each category for each G20 member. So for each G20 member, the number associated with each colour segment in the bar will be reported.
+#
+# 3. For each G20 member, focusing only on the harmful interventions implemented this year, please produce a 2-D 100% stacked bar which indicates the % of measures
+# that have already lapsed (by 21 October), the % of measures due to lapse during the remainder of this year, the % of measures due to lapse in 2021, the % of
+# measures due to lapse after 2021, and the % of measures without phase out dates.
+
+library(gtalibrary)
+library(tidyverse)
+library(ggplot2)
+library(ggrepel)
+library(gridExtra)
+library(scales)
+library(stringr)
+library(openxlsx)
+
+gta_setwd()
+gta26.path = "0 report production/GTA 26/"
+data.path = "data/Current year G20 measures/"
+out.path = "tables & figures/7 - Current year G20 measures/"
+source(paste0(gta26.path, "help files/GTA 26 cutoff and definitions.R"))
+
+gta_colour_palette()
+
+# Load data
+load(paste0(gta26.path, data.path, "current year g20 measures.Rdata"))
+
+### Save Excel files of the data
+write.xlsx(table1, file = paste0(gta26.path, out.path, "Figure 1 data.xlsx"))
+write.xlsx(table2, file = paste0(gta26.path, out.path, "Figure 2 data.xlsx"))
+write.xlsx(table3, file = paste0(gta26.path, out.path, "Figure 3 data.xlsx"))
+
+
+### Figure 1
+fig1 <- ggplot(data=table1)+
+  geom_line(aes(x=forcats::fct_inorder(as.character(year.published), ordered = T), y=total.nr.of.int, group=1), size=1, colour=gta_colour$qualitative[1])+
+  geom_line(aes(x=forcats::fct_inorder(as.character(year.published), ordered = T), y=harmful.perc*2800, group=1), size=1, colour=gta_colour$red[1]) +
+  scale_y_continuous(name=str_wrap("Total number of G20 commercial policy interventions worldwide documented by 21 October", 55),
+                     labels=seq(0,2800,400), limits = c(0,2800), breaks=seq(0,2800,400),
+                     sec.axis = sec_axis(trans = ~./2800, name=str_wrap("Percentage of G20 measures that harm trading partners", 55),
+                                         breaks=seq(0,1,0.1),labels=label_percent(accuracy = 1L)))+
+  labs(x = "", caption = "Source: Global Trade Alert.")+
+  gta_theme(x.bottom.angle = 90, x.bottom.align = 1) +
+  theme(legend.position = "bottom",
+        panel.grid.major.y = element_line(size=.1, color="#555555"),
+        panel.grid.minor.x = element_blank(),
+        panel.grid.minor.y = element_blank(),
+        plot.caption.position = "plot",
+        axis.text.y.left = element_text(colour = gta_colour$qualitative[1]),
+        axis.text.y.right = element_text(colour = gta_colour$red[1]))
+
+fig1
+
+gta_plot_saver(plot = fig1,
+               path = paste0(gta26.path, out.path),
+               name = "Figure 1 - G20 interventions documented by 21 October",
+               png = T,
+               pdf = T,
+               jpg = T)
+
+
+### Figure 2
+# Transform data to longer format
+table2 <- pivot_longer(table2, cols = c("nr.of.harmful.int", "nr.of.liberalising.int", "nr.of.cont.protection.int"), names_to = "intervention.type", values_to = "nr.of.interventions")
+
+# Plot
+fig2 <- ggplot(data = table2)+
+  geom_bar(aes(x=forcats::fct_reorder(implementing.jurisdiction, desc(-total.nr.of.interventions)), y=nr.of.interventions, fill=factor(intervention.type, levels=c("nr.of.cont.protection.int", "nr.of.liberalising.int", "nr.of.harmful.int"))), width=0.65, stat = "identity") +
+  geom_label(aes(x=forcats::fct_reorder(implementing.jurisdiction, desc(-total.nr.of.interventions)), y=nr.of.interventions, label=nr.of.interventions), colour="#3c3c3c", label.size = 0.2, label.padding = unit(0.15, "lines"), size=2.5, position = position_stack(vjust = 0.5)) +
+  labs(x="", y="", caption = "Source: Global Trade Alert.") +
+  scale_y_continuous(name=str_wrap("Number of measures recorded", 40), breaks=seq(0,275,25), labels=seq(0,275,25),
+                     sec.axis = sec_axis(trans = ~., name=str_wrap("Number of measures recorded", 40), breaks=seq(0,275,25), labels=seq(0,275,25)))+
+  scale_fill_manual(values=c("nr.of.harmful.int" = gta_colour$harmful[1], "nr.of.liberalising.int" = gta_colour$liberalising[1],"nr.of.cont.protection.int" = gta_colour$amber[1]), labels = c("contingent protection investigations", "liberalising interventions", "harmful interventions"))+
+  gta_theme(x.bottom.angle = 90, x.bottom.align = 0)+
+  guides(guide_legend(title = NULL, label.hjust = 0, label.vjust = 0.5, title.position = "top", title.vjust = 0.5, nrow = 2))+
+  theme(axis.text.x.bottom = element_text(hjust=1, size=10),
+        axis.title.y.left = element_text(size=10),
+        axis.title.y.right = element_text(size=10),
+        legend.text = element_text(size=9),
+        legend.title = element_blank(),
+        legend.key = element_rect(colour = "transparent", fill = "transparent"),
+        legend.box.background = element_rect(fill = "transparent", color="transparent"),
+        legend.position = "bottom",
+        legend.background = element_rect(fill="transparent", colour = "transparent"),
+        legend.margin = margin(t=10,b=0,r=100,l=100),
+        panel.grid.major.x = element_blank(),
+        panel.grid.major.y = element_line(size=.1, color=gta_colour$qualitative[1]),
+        panel.grid.minor.x = element_blank(),
+        panel.grid.minor.y = element_blank(),
+        panel.border = element_blank())
+
+fig2
+
+gta_plot_saver(plot = fig2,
+               path = paste0(gta26.path, out.path),
+               name = "Figure 2 - G20 interventions by evaluation and contingent protection investigations",
+               png = T,
+               pdf = T,
+               jpg = T,
+               width = 21,
+               height = 21)
+
+
+### Figure 3
+# Transform the data to longer format
+table3 <- pivot_longer(table3, cols = names(table3)[-1], names_to = "intervention.type", values_to = "perc.of.interventions")
+table3$implementing.jurisdiction <- as.character(table3$implementing.jurisdiction)
+
+# Plot
+fig3 <- ggplot(data = table3)+
+  geom_bar(aes(x=forcats::fct_inorder(implementing.jurisdiction, ordered = T), y=perc.of.interventions, fill=forcats::fct_inorder(intervention.type)), width=0.65, stat = "identity") +
+  labs(x="", y="", caption = "Source: Global Trade Alert.") +
+  scale_y_continuous(name=str_wrap("Percentage of measures recorded", 40), breaks=seq(0,1,0.1), labels=label_percent(accuracy = 1L),
+                     sec.axis = sec_axis(trans = ~., name=str_wrap("Percentage of measures recorded", 40), breaks=seq(0,1,0.1), labels=label_percent(accuracy = 1L)))+
+  scale_fill_manual(values=c("perc.of.lapsed.interventions" = gta_colour$qualitative[5], "perc.of.interventions.to.lapse.rest.of.year" = gta_colour$qualitative[4], "perc.of.interventions.to.lapse.2021" = gta_colour$qualitative[3],
+                             "perc.of.interventions.to.lapse.after.2021" = gta_colour$qualitative[2], "perc.of.interventions.without.phaseout.date" = gta_colour$qualitative[1]),
+                    labels = c("lapsed until 21 Oct", "lapses during the rest of 2020", "lapses in 2021",     
+                               "lapses after 2021", "has no phase-out date"))+
+  gta_theme(x.bottom.angle = 90, x.bottom.align = 0)+
+  guides(guide_legend(title = NULL, label.hjust = 0, label.vjust = 0.5, title.position = "top", title.vjust = 0.5, nrow = 2))+
+  theme(axis.text.x.bottom = element_text(hjust=1, size=10),
+        axis.title.y.left = element_text(size=10),
+        axis.title.y.right = element_text(size=10),
+        legend.text = element_text(size=9),
+        legend.title = element_blank(),
+        legend.position = "bottom",
+        panel.grid.major.x = element_blank(),
+        panel.grid.major.y = element_line(size=.1, color=gta_colour$qualitative[1]),
+        panel.grid.minor.x = element_blank(),
+        panel.grid.minor.y = element_blank(),
+        panel.border = element_blank())
+
+fig3
+
+gta_plot_saver(plot = fig3,
+               path = paste0(gta26.path, out.path),
+               name = "Figure 3 - G20 interventions by phase out date",
+               png = T,
+               pdf = T,
+               jpg = T,
+               height = 21,
+               width = 25)
